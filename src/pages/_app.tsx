b3f0ee2279b63b99ab4df63f208a95a3e3b9556a
Simlex <gettosimlex@gmail.com>
2023-04-14 17:40:14 +0100

    Updated App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <Component {...pageProps} />
+  </Layout>
 }
