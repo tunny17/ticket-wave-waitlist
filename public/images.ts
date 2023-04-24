114bc8481405d72c26fda0c5a289dcdab292d136
Simlex <gettosimlex@gmail.com>
2023-04-24 23:32:27 +0100

    Updated images assets

--- public/images.ts
+++ public/images.ts
@@ -1,6 +1,13 @@
 import logo from "./images/_logo.png";
+// import logo_white from "./images/logo_white.png";
+import logo_white from "./images/logo_white.svg";
+import hero_bg_1 from "./images/hero_bg_1.jpg";
+import user from "./images/user.jpg";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
   logo,
+  logo_white,
+  hero_bg_1,
+  user
 };
